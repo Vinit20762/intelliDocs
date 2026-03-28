b2e7c553db26b6b282e400a98198739262d402ff
Vinit Raj Singh <vinitrajsin@gmail.com>
2026-03-28 19:03:42 +0530

    navigation fixed in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,41 @@
 import FileUplaod from "@/components/FileUplaod";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
+import { desc, eq } from "drizzle-orm";
 import { LogIn } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+
+  let firstChat;
+  if (userId) {
+    firstChat = await db
+      .select()
+      .from(chats)
+      .where(eq(chats.userId, userId))
+      .orderBy(desc(chats.createdAt))
+      .limit(1);
+  }
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
-            <UserButton afterSignOutUrl="/" />
+            <UserButton />
           </div>
           <div className="flex mt-2">
-            {isAuth &&  <Button className="hover: cursor-pointer">Go to Chat</Button>}
+            {isAuth && firstChat && firstChat.length > 0 && (
+              <Link href={`/chat/${firstChat[0].id}`}>
+                <Button className="hover:cursor-pointer">Go to Chat</Button>
+              </Link>
+            )}
           </div>
            <p className="max-w-xl mt-1 text-lg text-slate-600">
             Join millions of students, researchers and professionals to instantly
